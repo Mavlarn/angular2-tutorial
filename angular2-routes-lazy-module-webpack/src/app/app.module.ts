6fb614e815700cdd055f0558845852fa7e43b797
Mavlarn <mavlarn@liepiaowang.com>
2016-12-14 11:16:22 +0800

    add lazy sub module example

--- angular2-routes-lazy-module-webpack/src/app/app.module.ts
+++ angular2-routes-lazy-module-webpack/src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
 import { AppRoutingModule } from './app.routes';
-import { TodoModule } from './todo/todo.module';
 
 import { AuthService } from './services/auth.service';
 
 import { Observable } from 'rxjs/Observable';
 
 @NgModule({
-    imports:        [BrowserModule, AppRoutingModule, TodoModule],
+    imports:        [BrowserModule, AppRoutingModule],
     declarations:   [AppComponent, HomeComponent],
     bootstrap:      [AppComponent],
     providers:      [AuthService]
